995258d3d7a2a6cf5ca89916caa320cf0a769f59
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-30 17:40:44 +0530

    Task13-show products in cart

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 //urlencoded + json encoded
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
